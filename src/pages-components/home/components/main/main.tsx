ccdb221d225621a5b149327c1b4a569b320a82eb
Shiraii92 <alexanderlippmann@hotmail.de>
2024-06-17 06:25:43 +0200

    New Backend API

--- src/pages-components/home/components/main/main.tsx
+++ src/pages-components/home/components/main/main.tsx
@@ -58,7 +58,7 @@ export const Main = () => {
   const updatePoints = () =>
     {
       const points = userStore((state) => state.points);
-      fetch("https://flipbackend-shiraii92s-projects.vercel.app/user/updatePoints/", {
+      fetch("https://miniapptest-backend2.vercel.app//user/updatePoints/", {
       // fetch("http://localhost:4000/user/updatePoints/", {
         method: 'POST',
         headers: {
