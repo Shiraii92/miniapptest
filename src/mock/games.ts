b045e89e2c31f836d190c9a6a5c84524c579d77a
Shiraii92 <alexanderlippmann@hotmail.de>
2024-06-29 22:19:52 +0200

    bugs fixed

--- src/mock/games.ts
+++ src/mock/games.ts
@@ -137,21 +137,21 @@ export const NEW_GAMES = [
         id: 1,
         img: "/avatars/avatar-1.png",
         count: 0,
-        index:0
+        index:0,
       },
       {
         user: "BRETT",
         img: "/avatars/avatar-2.png",
         count: 0,
         id: 2,
-        index:0
+        index:0,
       },
       {
         user: "BROTON",
         img: "/avatars/avatar-3.png",
         count: 0,
         id: 3,
-        index:1
+        index:1,
 
       },
       {
@@ -159,7 +159,7 @@ export const NEW_GAMES = [
         img: "/avatars/avatar-4.png",
         count: 0,
         id: 4,
-        index:1
+        index:1,
 
       },
       {
@@ -167,38 +167,38 @@ export const NEW_GAMES = [
         id: 1,
         img: "/avatars/avatar-5.png",
         count: 0,
-        index:2
+        index:2,
       },
       {
         user: "DOGE",
         img: "/avatars/avatar-6.png",
         count: 0,
         id: 2,
-        index:2
+        index:2,
       },
       {
         user: "DOGWIFHAT",
         img: "/avatars/avatar-7.png",
         count: 0,
         id: 3,
-        index:3
+        index:3,
       },
       {
         user: "FLIP",
         img: "/avatars/avatar-8.png",
         count: 0,
         id: 4,
-        index:3
+        index:3,
       },
     ],
   },
   {
     round: 2,
-    groups: [{index:0}, {index:0},{index:1},{index:1}],
+    groups: [{ index: 0 }, { index: 0 }, { index: 1 }, { index: 1 }],
   },
   {
     round: 3,
-    groups: [{index:0},{index:0}],
+    groups: [{ index: 0 }, { index: 0 }],
   },
   {
     round: 4,
--- src/mock/profile.ts
+++ src/mock/profile.ts
@@ -1,14 +1,14 @@
 export const ACTIVE_WAGERS = [
   {
     date: "Match 1",
-    firstuser: "Ton",
-    seconduser: "Sol",
+    firstuser: "TON",
+    seconduser: "SOLANA",
     point: 500,
   },
   {
     date: "Match 2",
-    firstuser: "Kate",
-    seconduser: "Jane",
+    firstuser: "FLIP",
+    seconduser: "JOHNDOGE",
     point: 300,
   },
 ];
