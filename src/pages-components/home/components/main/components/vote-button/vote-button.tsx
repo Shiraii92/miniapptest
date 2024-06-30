bb923579d3505f45a909ce833892ac679c5f2fad
Shiraii92 <alexanderlippmann@hotmail.de>
2024-06-30 23:06:47 +0200

    bugs fixed

--- src/pages-components/home/components/main/components/vote-button/vote-button.tsx
+++ src/pages-components/home/components/main/components/vote-button/vote-button.tsx
@@ -4,7 +4,7 @@ import styles from "./vote-button.module.css";
 import { Coin } from "@/common";
 // import { Props } from "../vote-button/types";
 import { useEffect, useState } from "react";
-import {userStore} from '../../../../../../stores/store';
+import { userStore } from '../../../../../../stores/store';
 
 type Props = {
   womenId: number,
@@ -15,6 +15,7 @@ export const VoteButton = ({ womenId }: Props) => {
   let points = userStore((state) => state.points)
   const addPoint = userStore((state) => state.addPoint);
   const addVote = userStore((state) => state.addVote);
+  const username = userStore((state) => state.username);
 
   useEffect(() => {
     if (click) {
@@ -26,15 +27,39 @@ export const VoteButton = ({ womenId }: Props) => {
     }
   }, [click]);
 
+  const handleVote = async () => {
+    try {
+      const response = await fetch("https://miniapptest-backend2.vercel.app/user/addVote", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          womenId: womenId,
+          username: username,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      addPoint(points++);  // assuming the response returns the new points value
+      addVote(womenId); // assuming the response returns necessary vote information
+    } catch (error) {
+      console.error('There was a problem with your vote operation:', error);
+    }
+  };
+
   return (
     <div
       onClick={(e) => {
-      if (e.target instanceof Element && e.target.closest(`.${styles.vote_button_bg}`)) {
-        addPoint(points++);
-        addVote(womenId);
-        setClick(true);
-      }
-    }}
+        if (e.target instanceof Element && e.target.closest(`.${styles.vote_button_bg}`)) {
+          handleVote();
+          setClick(true);
+        }
+      }}
       className={`${styles.vote_button_bg} flex flex-col justify-center items-center w-[40px] cursor-pointer h-[40px] absolute top-[-20px] right-[-20px] z-[1000] rounded-full `}
     >
       <span className='animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75'></span>
